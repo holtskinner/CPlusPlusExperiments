38dc213ffb94d92001de6c08c197358a695608f8
Holt Skinner <HoltWasHere@gmail.com>
2016-12-31 11:18:49 -0600

    Added First 3 Labs/Gitignore

---
+++ Lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double x = 0, y = 0, z = 0;
+
+  cout << "Enter 3 numbers to add\n";
+  cin >> x >> y >> z;
+
+  cout << "The sum of the numbers is " << x + y + z << "\n";
+  return 0;
+}
---
+++ Lab2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double a = 0, b = 0, c = 0, s = 0;
+
+  cout << "Enter 3 sides of a triangle\n";
+  cin >> a >> b >> c;
+
+  while (a <= 0) {
+    cout << "Side 1 is invalid, Try again.\n";
+    cin >> a;
+  }
+
+  while (b <= 0) {
+    cout << "Side 2 is invalid, Try again.\n";
+    cin >> b;
+  }
+
+  while (c <= 0) {
+    cout << "Side 3 is invalid, Try again.\n";
+    cin >> c;
+  }
+
+  if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
+    cout << "Sides do not form a triangle.\n";
+    return 0;
+  }
+
+  s = (a + b + c) / 2;
+
+  cout << "Area of triangle is " << sqrt(s * (s - a) * (s - b) * (s - c))
+       << "\n";
+
+  if (a == b && b == c) {
+    cout << "Triangle is equilateral.\n";
+  }
+  if (a == b || b == c || a == c) {
+    cout << "Triangle is isoseles.\n";
+  } else if (a != b != c) {
+    cout << "Triangle is scalene.\n";
+  }
+
+  return 0;
+}
